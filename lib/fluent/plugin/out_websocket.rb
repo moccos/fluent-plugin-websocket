cd47187aab94f821488f46c086d296277a69e18a
moccos <tt.izawa@gmail.com>
2013-04-13 21:36:00 +0900

    Added simple sampling feature. next: document and test

--- lib/fluent/plugin/out_websocket.rb
+++ lib/fluent/plugin/out_websocket.rb
@@ -26,32 +26,61 @@ module Fluent
     config_param :port, :integer, :default => 8080
     config_param :add_time, :bool, :default => false
     config_param :add_tag, :bool, :default => true
+    config_param :sampling_interval, :integer, :default => 0
+    config_param :sampling_probability, :integer, :default => -1
 
+    ### private methods
+    def make_filter()
+      if (@sampling_interval > 0) then
+        @interval_counter = 0
+        lambda {||
+          @interval_counter = (@interval_counter + 1) % @sampling_interval
+          (@interval_counter == 0)
+        }
+      elsif (@sampling_probability > 0) then
+        lambda {||
+          #r = Random.rand(1 .. 100)
+          #puts @sampling_probability.to_s + " / " + r.to_s
+          #@sampling_probability > r
+          @sampling_probability > Random.rand(1 .. 100)
+        }
+      else
+        lambda {|| true}
+      end
+    end
+    private :make_filter
+
+    def ws_open(ws)
+      ws.onopen { |handshake|
+        callback = @use_msgpack ? proc{|msg| ws.send_binary(msg)} : proc{|msg| ws.send(msg)}
+        $lock.synchronize do
+          sid = $channel.subscribe callback
+          $log.trace "WebSocket connection: ID " + sid.to_s
+          ws.onclose {
+            $log.trace "Connection closed: ID " + sid.to_s
+            $lock.synchronize do
+              $channel.unsubscribe(sid)
+            end
+          }
+        end
+
+        #ws.onmessage { |msg|
+        #}
+      }
+    end
+    private :ws_open
+
+    ### Fluentd interface methods
     def configure(conf)
       super
       $thread = Thread.new do
       $log.trace "Started em-websocket thread."
       $log.info "WebSocket server #{@host}:#{@port} [msgpack: #{@use_msgpack}]"
+      @filter = make_filter()
       EM.run {
-        EM::WebSocket.run(:host => @host, :port => @port) do |ws|
-          ws.onopen { |handshake|
-            callback = @use_msgpack ? proc{|msg| ws.send_binary(msg)} : proc{|msg| ws.send(msg)}
-            $lock.synchronize do
-              sid = $channel.subscribe callback
-              $log.trace "WebSocket connection: ID " + sid.to_s
-              ws.onclose {
-                $log.trace "Connection closed: ID " + sid.to_s
-                $lock.synchronize do
-                  $channel.unsubscribe(sid)
-                end
-              }
-            end
-
-            #ws.onmessage { |msg|
-            #}
-          }
-        end
+        EM::WebSocket.run(:host => @host, :port => @port) do |ws| ws_open(ws) end
       }
+      
       end
     end
 
@@ -73,8 +102,10 @@ module Fluent
         if (@add_time) then data.unshift(time) end
         if (@add_tag) then data.unshift(tag) end
         output = @use_msgpack ? data.to_msgpack : data.to_json
-        $lock.synchronize do
-          $channel.push output
+        if @filter != nil and @filter.() then
+          $lock.synchronize do
+            $channel.push output
+          end
         end
       }
     end
